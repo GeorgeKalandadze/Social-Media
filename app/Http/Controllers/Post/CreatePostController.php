910709c4248b9ff2886bff3307aa4098865cbad0
George Kalandadze <kalandadzeg777@gmail.com>
2023-08-06 23:32:06 +0400

    feat:make json resources for post and post images

--- app/Http/Controllers/Post/CreatePostController.php
+++ app/Http/Controllers/Post/CreatePostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Post;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\PostRequest;
+use App\Http\Resources\PostResource;
 use App\Models\Post;
 use App\Models\PostImage;
 use Illuminate\Http\JsonResponse;
@@ -12,7 +13,7 @@ use Illuminate\Support\Facades\Storage;
 
 class CreatePostController extends Controller
 {
-    public function __invoke(PostRequest $request): JsonResponse
+    public function __invoke(PostRequest $request): PostResource
     {
         try {
             DB::beginTransaction();
@@ -37,8 +38,7 @@ class CreatePostController extends Controller
             }
             DB::commit();
             $post->load('postImages');
-            return response()->json($post);
-
+            return new PostResource($post);
         }catch (\Exception $exception)
         {
             DB::rollBack();
