a7568e92fbe7cb2af8c3ae750dbee3624aedca76
George Kalandadze <kalandadzeg777@gmail.com>
2023-08-14 19:20:15 +0400

    feat:add post_id for comment and make functionality of post creation

--- app/Http/Controllers/Comment/CreateCommentController.php
+++ app/Http/Controllers/Comment/CreateCommentController.php
@@ -3,11 +3,22 @@
 namespace App\Http\Controllers\Comment;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CommentRequest;
+use App\Models\Comment;
 
 class CreateCommentController extends Controller
 {
-    public function __invoke()
+    public function __invoke(CommentRequest $request)
     {
+        $data = $request->validated();
+        $user = $request->user();
+        $comment = Comment::create([
+            'user_id' => $user->id,
+            'post_id' => $data->post_id,
+            'body' => $data->body,
+            'parent_comment_id' => $data->parent_comment_id,
+        ]);
 
+        return response()->json($comment);
     }
 }
