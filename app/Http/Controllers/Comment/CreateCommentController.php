57cda121d09c821912dec02fe19c761b2cd04e51
George Kalandadze <kalandadzeg777@gmail.com>
2023-08-14 19:10:27 +0400

    feat:make comment request file and create comment controller

---
+++ app/Http/Controllers/Comment/CreateCommentController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Comment;
+
+use App\Http\Controllers\Controller;
+
+class CreateCommentController extends Controller
+{
+    public function __invoke()
+    {
+
+    }
+}
